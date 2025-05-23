9b61f78a46c8afdcc0b6f175d724006ff3523a36
Patric Pauluk <patric.pauluk@outlook.com>
2025-05-22 23:43:24 -0300

    Preparando para deploy no GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/projeto-rota/',
   plugins: [react()],
   server: {
     port: 5173,
